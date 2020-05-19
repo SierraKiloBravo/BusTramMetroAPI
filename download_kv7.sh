38ee5a850fda17c8a2fbbdda653305ffb0efe7af
SierraKiloBravo <65400576+SierraKiloBravo@users.noreply.github.com>
2020-05-19 17:04:12 +0200

    typo

--- download_kv7.sh
+++ download_kv7.sh
@@ -7,7 +7,7 @@ mkdir -p kv78dl/KV7planning
 wget -P kv78dl/KV7calendar/ $(curl http://kv7.openov.nl/GOVI/KV7calendar/ |   grep -io '<a class=\"link\" href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a class=\"link\" href=["'"'"']//i' -e 's/["'"'"']$//i' -e 's/..\///' -e '/^\s*$/d' -e 's/^/http:\/\/kv7.openov.nl\/GOVI\/KV7calendar\//')
 
 # Download planning messages, only the last 2500 as kv7.openov.nl/GOVI/KV7planning goes back to 2018 which is completely unnecessary
-wget -P kv78dl/KV7planning/ $(cat curl http://kv7.openov.nl/GOVI/KV7planning/ |   grep -io '<a class=\"link\" href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a class=\"link\" href=["'"'"']//i' -e 's/["'"'"']$//i' -e 's/^/http:\/\/kv7.openov.nl\/GOVI\/KV7planning\//' | tail -n1500
+wget -P kv78dl/KV7planning/ $(cat curl http://kv7.openov.nl/GOVI/KV7planning/ |   grep -io '<a class=\"link\" href=['"'"'"][^"'"'"']*['"'"'"]' |   sed -e 's/^<a class=\"link\" href=["'"'"']//i' -e 's/["'"'"']$//i' -e 's/^/http:\/\/kv7.openov.nl\/GOVI\/KV7planning\//' | tail -n1500)
 
 # decompress messages
 gunzip kv78dl/KV7calendar/*.gz
